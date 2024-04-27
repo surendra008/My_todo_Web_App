4f6755decebc642f85e6c7eb61370774fc067123
Surendra_repo <surireddy819@gmail.com>
2024-04-27 16:40:44 +0530

    ignored venv folder

--- web.py
+++ web.py
@@ -2,13 +2,19 @@ import  streamlit as st
 import functions as fu
 
 todos = fu.get_todos()
+
+st.set_page_config(layout="wide")
 def add_todo():
     todo = st.session_state["new_todo"]+ "\n"
     todos.append(todo)
     fu.write_todos(todos)
 
 st.title('My Todo APP')
-st.subheader('Help to listout your todo list')
+st.subheader('This is My Todo APP')
+#st.sidebar.title('My Todo APP')
+
+st.text_input(label="", placeholder="Add a todo item",on_change=add_todo,key="new_todo")
+
 
 for index,todo in enumerate(todos):
     checkbox=st.checkbox(todo,key=todo)
@@ -19,4 +25,3 @@ for index,todo in enumerate(todos):
         st.experimental_rerun()
 
 
-st.text_input(label="", placeholder="Add a todo item",on_change=add_todo,key="new_todo")
